6763dae6471f4677c5cb1bebf07ac7226e6d1f9b
karma gyatso <karma@karmas-MacBook-Pro.local>
2021-03-30 15:59:36 -0400

    task tracker added

---
+++ taskTracker/src/app/tasklist.model.ts
@@ -0,0 +1,9 @@
+export class TasklistModel {
+  //this class is use to map jason data
+  constructor(
+    public id: any,
+    public name: string,
+    public task: string,
+    public date: any
+  ) {}
+}
